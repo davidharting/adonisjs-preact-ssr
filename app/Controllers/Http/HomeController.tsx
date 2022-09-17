858e7d692192d0fd84856138d01ca24636a4c8f7
David Harting <david.e.harting@gmail.com>
2022-09-17 09:59:19 -0400

    Use jsx

--- app/Controllers/Http/HomeController.tsx
+++ app/Controllers/Http/HomeController.tsx
@@ -2,25 +2,27 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import type { VNode, FunctionComponent } from 'preact'
 
 import { render as preactRenderToString } from 'preact-render-to-string'
-import { html } from 'htm/preact'
 
 export default class HomeController {
   public async show(ctx: HttpContextContract) {
     const query = ctx.request.qs()
     const name = query['name'] || 'friend'
-    const rendered = html`<div>${HomePage({ name, children: html`<h3>I am child</h3>` })}</div>`
-    return render(ctx, rendered)
+    return render(ctx, <HomePage name={name} />)
   }
 }
 
 type HomePageProps = {
   name: string
 }
-const HomePage: FunctionComponent<HomePageProps> = ({ name, children }) => html`
-  <h1>Hello, ${name}!</h1>
-  ${children}
-  <p>This is happening in preact</p>
-`
+const HomePage: FunctionComponent<HomePageProps> = ({ name, children }) => {
+  return (
+    <>
+      <h1>Hello, {name}!</h1>
+      {children}
+      <p>This is happening in preact</p>
+    </>
+  )
+}
 
 type RenderOptions = {
   layout?: string
