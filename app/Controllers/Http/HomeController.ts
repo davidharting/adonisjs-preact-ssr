a61c10a6f36097499c4289ce437058aa591c8395
David Harting <david.e.harting@gmail.com>
2022-09-16 21:14:49 -0400

    Add component usage
    I am using function infocation rather than <${Component}> style, because I want the typesafety for the components

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -8,23 +8,24 @@ export default class HomeController {
   public async show(ctx: HttpContextContract) {
     const query = ctx.request.qs()
     const name = query['name'] || 'friend'
-    const vnode = HomePage({ name })
-    return render(ctx, vnode)
+    const rendered = html`<div>${HomePage({ name, children: html`<h3>I am child</h3>` })}</div>`
+    return render(ctx, rendered)
   }
 }
 
 type HomePageProps = {
   name: string
 }
-const HomePage: FunctionComponent<HomePageProps> = ({ name }) => html`
+const HomePage: FunctionComponent<HomePageProps> = ({ name, children }) => html`
   <h1>Hello, ${name}!</h1>
+  ${children}
   <p>This is happening in preact</p>
 `
 
 type RenderOptions = {
   layout?: string
 }
-const render = (ctx: HttpContextContract, component: VNode | null, options: RenderOptions = {}) => {
+const render = (ctx: HttpContextContract, component: VNode, options: RenderOptions = {}) => {
   const layout = options.layout ?? 'layout/app'
   let contents = ''
   if (component) {
